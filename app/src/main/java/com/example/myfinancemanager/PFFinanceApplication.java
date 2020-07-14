61d52c39427455f248818ed15cae8ed3fb86877d
DRAGON <baran39260@gmail.com>
2020-07-14 20:00:00 +0430

    Initial commit

---
+++ app/src/main/java/com/example/myfinancemanager/PFFinanceApplication.java
@@ -0,0 +1,17 @@
+package com.example.myfinancemanager;
+
+import android.app.Application;
+
+import com.example.myfinancemanager.utility.TypefaceUtil;
+
+import net.danlew.android.joda.JodaTimeAndroid;
+
+public class PFFinanceApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JodaTimeAndroid.init(this);
+        TypefaceUtil.overrideFont(this,"SERIF","fonts/EstedadFDMedium.ttf");
+    }
+}
